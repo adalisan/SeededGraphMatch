af7f36e158f09924f010ed768038602546cdc4fb
adali <adali@2288d9a7-0fa1-3145-9870-04f04981ec47>
2012-07-26 18:30:22 +0000

    Former-commit-id: 738fec4e3fda44271ae8d5b5aaad9f48265415ae

--- src/experiment_lap_jofc_group.m
+++ src/experiment_lap_jofc_group.m
@@ -3,6 +3,11 @@ function [pc,pcjofc] = experiment(n,maxm,numiter)
 pc=zeros(1,maxm+1);
 pcjofc = zeros(1,maxm+1);
 truematch = 0;
+
+num_w =1;
+w_wals_vec = zeros(num_w,1);
+w_vals_vec(1) = 0.95;
+
 for i=1:numiter
     i
     Bernoulli=rand(maxm+n,maxm+n);
@@ -16,40 +21,60 @@ for i=1:numiter
         bij=graphmatchHARDSEED(At,Bt,j);
         pc(j+1)=pc(j+1)+sum(bij(j+1:j+n)'==[j+1:j+n])/n;
         embed.dim =2;
-        if (j<=2) 
+        if (j<=2)
             continue
         else
             embed.dim = ceil( j/2) ;
-        
-        end
             
+        end
+        
         putRdata('At',At)
         putRdata('Bt',Bt)
         insample_logic_vector = logical([ones(1,j) zeros(1,n) ]');
         putRdata('insample_logic_vec', insample_logic_vector)
         putRdata('embed.dim', embed.dim)
+        
         evalR('insample_logic_vec<-as.vector(insample_logic_vec)')
-          evalR('insample_logic_vec<-rep(insample_logic_vec,2)')
-          evalR('sink("debug.mat.txt")')
-      %evalR('traceback()')
-      evalR('print(insample_logic_vec)')
-      evalR('sink()')
+        evalR('insample_logic_vec<-rep(insample_logic_vec,2)')
+        putRdata('w_vals_vec',w_vals_vec)
+        evalR('sink("debug.mat.txt")')
+        %evalR('traceback()')
+        evalR('print(insample_logic_vec)')
+        evalR('sink()')
         %evalR('error.handle <- function()')
         evalR(['error.handle <- function(ex) {print(ex)}'])
-        evalR('jofc.result <- try(jofc(G=At,Gp=Bt, in.sample.ind=insample_logic_vec,  d.dim=embed.dim,w.vals.vec=0.99,graph.is.directed=FALSE, oos=TRUE,use.weighted.graph=FALSE))')
-       % evalR('eval(parse(jofc.call))')
+        evalR('jofc.result <-try(JOFC.graph.custom.dist(G=At,Gp=Bt, in.sample.ind=insample_logic_vec,  d.dim=embed.dim,w.vals.vec=w_vals_vec,graph.is.directed=FALSE,vert_diss_measure="default"))')
+        % evalR('jofc.result <- try(jofc(G=At,Gp=Bt, in.sample.ind=insample_logic_vec,  d.dim=embed.dim,w.vals.vec=0.99,graph.is.directed=FALSE, oos=TRUE,use.weighted.graph=FALSE))')
+        % evalR('eval(parse(jofc.call))')
         %evalR('jofc.result<- tryCatch ({eval(parse(jofc.call))},error=error.handle)')
-      %  traceback   })')
-      evalR('sink("debug.matlab.txt")')
-      evalR('traceback()')
-      evalR('print(jofc.result)')
-      evalR('sink()')
+        %  traceback   })')
+        
+        evalR('skip.iter<-FALSE')
+        evalR('	skip.iter <-inherits(jofc.result,"try-error")')
+        if (getRdata('skip.iter'))
+            j
+            evalR('sink("debug.matlab.txt")')
+            evalR('traceback()')
+            evalR('print(jofc.result)')
+            evalR('sink()')
+            
+            'Skipping iteration'
+            continue
+            
+        end
         evalR('jofc.res.1<-jofc.result[[1]]')
         getRdata('jofc.res.1') ;
         evalR('M.result<-try(solveMarriage(jofc.res.1))')
-        evalR('skip.iter<-FALSE')
         evalR('	skip.iter <-inherits(M.result,"try-error")')
-        if (getRdata('skip.iter')) 
+        if (getRdata('skip.iter'))
+            j
+            'M.result error'
+            evalR('sink("debug.matlab.2.txt")')
+            evalR('traceback()')
+            evalR('print(M.result)')
+            evalR('sink()')
+            
+            
             'Skipping iteration'
             continue
             
