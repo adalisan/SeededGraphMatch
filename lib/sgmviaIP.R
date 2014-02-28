0feb7c16ebba39da59763a3a56c181bbb5a5be31
Sancar <debroglie81@hotmail.om>
2014-02-28 00:36:54 -0500

    fix sgm Via Int Program
    change c(r)bind function to Matrix-compatible c(r)Bind

--- lib/sgmviaIP.R
+++ lib/sgmviaIP.R
@@ -1,43 +1,43 @@
 sgmViaIP <- function (A, B,m){
   require(Matrix)
   
-  n = nrow(A);
-  test.v.count= n-m;
+  totv = nrow(A);
+  n= totv-m;
   A11=A[1:m,1:m];
-  A12=A[1:m,m+(1:test.v.count)];
-  A21=A[m+(1:test.v.count),1:m];
-  A22=A[m+(1:test.v.count),m+(1:test.v.count)];
+  A12=A[1:m,m+(1:n)];
+  A21=A[m+(1:n),1:m];
+  A22=A[m+(1:n),m+(1:n)];
   B11=B[1:m,1:m];
-  B12=B[1:m,m+(1:test.v.count)];
-  B21=B[m+(1:test.v.count),1:m];
-  B22=B[m+(1:test.v.count),m+(1:test.v.count)];
+  B12=B[1:m,m+(1:n)];
+  B21=B[m+(1:n),1:m];
+  B22=B[m+(1:n),m+(1:n)];
   
   vecB12=B12
-  dim(vecB12)<- c(m*test.v.count,1);
+  dim(vecB12)<- c(m*n,1);
 
   vecA21=A21 ;
-  dim(vecA21)<- c(m*test.v.count,1);
-  eye.test.v.count<-diag(test.v.count)
-  eye.test.v.countsq.2mtest.v.count = diag(test.v.count^2+2*m*test.v.count)
-  vec.1 <- matrix(1,1,test.v.count) 
-  M11=rbind( eye.test.v.count%x% A22- t(B22) %x% eye.test.v.count, 
-      eye.test.v.count %x% A12, t(B21) %x% eye.test.v.count);
-  M21= rbind( eye.test.v.count %x% vec.1 , vec.1 %x% eye.test.v.count);
-  M=rbind(cbind( M11, eye.test.v.countsq.2mtest.v.count , -eye.test.v.countsq.2mtest.v.count),
-          cbind(M21,  matrix(0,2*test.v.count,2*test.v.count^2+4*m*test.v.count))
+  dim(vecA21)<- c(m*n,1);
+  eye.n<-diag(n)
+  eye.nsq.2mn = diag(n^2+2*m*n)
+  vec.1 <- matrix(1,1,n) 
+  M11=rBind( (eye.n%x% A22)- (t(B22) %x% eye.n), 
+      eye.n %x% A12, t(B21) %x% eye.n);
+  M21= rBind( eye.n %x% vec.1 , vec.1 %x% eye.n);
+  M=rBind(cBind( M11, eye.nsq.2mn , -eye.nsq.2mn),
+          cBind(M21,  matrix(0,2*n,2*n^2+4*m*n))
           ); 
-  f=cbind( matrix(0,1,test.v.count^2) , matrix(1,1, 2*test.v.count^2+4*m*test.v.count) );
-  Mineq= rep(0,3*test.v.count^2+4*m*test.v.count);
+  f=cBind( matrix(0,1,n^2) , matrix(1,1, 2*n^2+4*m*n) );
+  Mineq= rep(0,3*n^2+4*m*n);
   M=Matrix(M,sparse=TRUE);
-  b=rbind(matrix(0,test.v.count^2,1) , vecB12 , vecA21 , matrix(1,2*test.v.count,1));
+  b=rBind(matrix(0,n^2,1) , vecB12 , vecA21 , matrix(1,2*n,1));
   
-  sense_eq = rep('=',test.v.count^2+2*m*test.v.count+2*test.v.count);
- # setest.v.countse_itest.v.counteq = rep('<',0, 1];
-  #setest.v.countse = [sense_eq; setest.v.countse_itest.v.counteq];
+  sense_eq = rep('=',n^2+2*m*n+2*n);
+ # sense_ineq = rep('<',0, 1];
+  #sense = [sense_eq; sense_ineq];
    sense = sense_eq
-  #Bitest.v.countary atest.v.countd Real Variables
-  vtype1 = rep('B',test.v.count^2);
-  vtype2 = rep('B',2*test.v.count^2+4*m*test.v.count);
+  #Binary and Real Variables
+  vtype1 = rep('B',n^2);
+  vtype2 = rep('B',2*n^2+4*m*n);
   vtype = c(vtype1, vtype2);
   
   model = list()
@@ -53,10 +53,10 @@ sgmViaIP <- function (A, B,m){
   x = round(x);
   
   P=x;
-  dim(P)<- c(test.v.count,test.v.count)
-  P.full <- diag(n)
-  P.full[(m+1):n,(m+1):n] <- P
-  temp=P%*%matrix(1:test.v.count, test.v.count, 1);
+  dim(P)<- c(n,n)
+  P.full <- diag(totv)
+  P.full[(m+1):totv,(m+1):totv] <- P
+  temp=P%*%matrix(1:n, n, 1);
    alignment=c( 1:m , t(temp+m) );
   
    return (list(matching=alignment,matching.seeded=t(temp+m), 
