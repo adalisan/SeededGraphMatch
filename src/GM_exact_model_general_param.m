813257cfcc639d099b5043f9c9d41aeab9ea1d14
Sancar Adali <sadali1@jhu.edu>
2014-04-05 02:34:54 -0400

    small sanity checks

--- src/GM_exact_model_general_param.m
+++ src/GM_exact_model_general_param.m
@@ -1,10 +1,30 @@
 
+
+addpath('~/projects/gurobi/gurobi562/linux64/matlab');
+if  ~exist('p') 
+    p=0.2;
+end
+if  ~exist('corr') 
+    corr = 0.7;
+end
+
+if  ~exist('m') 
+    m=10;
+end
+if  ~exist('n') 
+    n=45;
+    
+end
+mplusn = m+n;
+
+
+
 [G_1,G_2,true_perm] = generate(p*ones(mplusn),corr,m);
 
 if ~exist('tag') 
     tag='_';
 end
 
-model_fname_id = strcat(int2str(mplusn),'verts_edgeprob_',p, '_corr_coef_pt',num2str(corr),'_',m,'seeds',tag)
+model_fname_id = strcat(int2str(mplusn),'verts_edgeprob_',p, '_corr_coef_pt',num2str(100*corr),'_',m,'seeds',tag)
 fname_1= GM_exact_model_writer  (G_1,G_2,m,model_fname_id)
 fname_2=GM_exact_model_writer_infeasible  (G_1,G_2,m,model_fname_id)
