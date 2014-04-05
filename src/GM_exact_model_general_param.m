e8f8fec7fd56075b50d65a3d8384a0bcfce00d60
Sancar Adali <sadali1@jhu.edu>
2014-04-05 02:37:25 -0400

    small fix with model fname

--- src/GM_exact_model_general_param.m
+++ src/GM_exact_model_general_param.m
@@ -25,6 +25,6 @@ if ~exist('tag')
     tag='_';
 end
 
-model_fname_id = strcat(int2str(mplusn),'verts_edgeprob_',p, '_corr_coef_pt',num2str(100*corr),'_',m,'seeds',tag)
+model_fname_id = strcat(int2str(mplusn),'verts_edgeprob_pt',int2str(100*p), '_corr_coef_pt',int2str(100*corr),'_',m,'seeds',tag)
 fname_1= GM_exact_model_writer  (G_1,G_2,m,model_fname_id)
 fname_2=GM_exact_model_writer_infeasible  (G_1,G_2,m,model_fname_id)
