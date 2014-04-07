180cfff83eb6ebf74d51565a6f82891ea5d218a6
Sancar Adali <sadali1@jhu.edu>
2014-04-07 01:38:12 -0400

    try to use  perturbation for generate EM to fix problems with pymatlab

--- src/generate.m
+++ src/generate.m
@@ -8,27 +8,35 @@ function [A,B,truth]=generate(Bern,crln,m)
 
 [nplusm,~]=size(Bern);
 n=nplusm-m;
+% 
+% A=zeros(nplusm,nplusm);
+% 
+% B=zeros(nplusm,nplusm);
+% for i=1:nplusm
+%     for j=i+1:nplusm
+%         Bern_tmp = double(Bern(i,j));
+%         A(i,j)=    (rand<Bern_tmp);
+%         A(j,i)=A(i,j);
+%         A_tmp= double(A(i,j));
+%         
+%         B(i,j)=    (rand< (  (1.0-crln)*Bern_tmp+crln*A_tmp )  );
+%         B(j,i)=B(i,j);
+%     end
+% end
 
-A=zeros(nplusm,nplusm);
+ Bernoulli=rand(N);
+        A=rand(N)<Bernoulli;
+        A=A-triu(A);A=A+A';
+        B=A;
+        B=bitflip(A,rho_val);
+        B=B-triu(B);B=B+B';
 
-B=zeros(nplusm,nplusm);
-for i=1:nplusm
-    for j=i+1:nplusm
-        Bern_tmp = double(Bern(i,j));
-        A(i,j)=    (rand<Bern_tmp);
-        A(j,i)=A(i,j);
-        A_tmp= double(A(i,j));
-        
-        B(i,j)=    (rand< (  (1.0-crln)*Bern_tmp+crln*A_tmp )  );
-        B(j,i)=B(i,j);
-    end
-end
 
 
 mix=[ [1:m] m+randperm(n) ];
 A=A(mix,mix);
 B=B(mix,mix);
-truth=mix;
+truth=[ [1:m] m+randperm(n) ];
 A=A(truth,truth);
 
 
