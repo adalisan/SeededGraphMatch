9a0dd83c6558fafd5b721b20be2026ca66286694
Sancar Adali <sadali1@jhu.edu>
2014-04-08 20:40:40 -0400

    fix values for m,mplusn and n return both # of True Matchings and fraction of TM

--- lib/PermMat2TrueMatch.m
+++ lib/PermMat2TrueMatch.m
@@ -1,7 +1,9 @@
-function [alignment]= PermMat2TrueMatch(P,true_alignment)
-
-[mplusn,~] =  size(P);
-temp=P*[1:mplusn]';
-alignment=[ [1:m] temp'+m ];
-mplusn = len(alignment);
-sum(true_alignment[(m+1):mplusn] == alignment[(m+1):mplusn])
+function [truematch,truematch_ratio]= PermMat2TrueMatch(P,true_alignment,m)
+    [n,~] =  size(P);
+    mplusn = m+n; 
+    temp=P*[1:mplusn]';
+    alignment=[ [1:m] temp'+m ];
+    mplusn = len(alignment);
+    truematch = sum(true_alignment[(m+1):mplusn] == alignment[(m+1):mplusn]);
+    truematch_ration = truematch/(n)
+    
